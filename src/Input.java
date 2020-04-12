ff8df58c57e2b9b3e5de4696f1ead9b8f9ac6b0e
Andrew Ha <andrewha428@gmail.com>
2020-04-11 21:22:58 -0400

    Fixed bugs with regex for inputting date and phone number

--- src/Input.java
+++ src/Input.java
@@ -1,19 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.InputMismatchException;
 import java.util.NoSuchElementException;
-import java.util.Scanner;
+import java.util.function.DoubleToIntFunction;
+import java.util.regex.Pattern;
 
 public class Input {
 
-    private static Scanner scanner = new Scanner(System.in);
+    private static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+    private static String readInput() {
+        while (true) {
+            try {
+                String line = in.readLine();
+                return line.trim();
+            } catch (IOException e) {
+                System.out.println("There was an error reading your input");
+            }
+        }
+    }
 
     public static int getInt() {
         while(true) {
             try {
-                int output = scanner.nextInt();
-                scanner.nextLine();
+                String input = readInput();
+                int number = Integer.parseInt(input);
 
-                return output;
-            } catch (InputMismatchException ex) {
+                return number;
+            } catch (NumberFormatException ex) {
                 System.out.println("Please enter a valid integer");
             }
         }
@@ -22,57 +37,48 @@ public class Input {
     public static double getDouble() {
         while(true) {
             try {
-                double output = scanner.nextDouble();
-                scanner.nextLine();
+                String input = readInput();
+                double number = Double.parseDouble(input);
 
-                return output;
-            } catch (InputMismatchException ex) {
+                return number;
+            } catch (NumberFormatException ex) {
                 System.out.println("Please enter a valid double");
             }
         }
     }
 
     public static String getString() {
-        while(true) {
-            try {
-                String output = scanner.nextLine();
-                scanner.nextLine();
-
-                return output;
-            } catch (NoSuchElementException ex) {
-                System.out.println("Error reading new line, please try again");
-            }
-        }
+        return readInput();
     }
 
     public static String getPhoneNumber() {
+        System.out.println("Please enter a phone number with the following format: XXX-XXX-XXXX");
+
         while(true) {
-            try {
-                String phoneNum = scanner.nextLine();
+            String phoneNum = readInput();
 
-                //Matching XXX-XXX-XXXX Format through regex expression
-                if (phoneNum.matches("\\\\d{3}[-\\\\.\\\\s]\\\\d{3}[-\\\\.\\\\s]\\\\d{4}")) {
-                    return phoneNum;
-                }
-
-            } catch (NoSuchElementException ex) {
-                System.out.println("Error reading phone number, please try again");
+            //Matching XXX-XXX-XXXX Format through regex expression
+            if (phoneNum.matches("(?:\\d{3}-){2}\\d{4}")) {
+                return phoneNum;
             }
+
+            System.out.println("The phone number is in the wrong format: Please try again");
+
         }
     }
 
     public static String getDate() {
-        while (true) {
-            try {
-                String date = scanner.nextLine();
+        System.out.println("Please enter a date with the following format: MM/DD/YYYY");
 
-                //Matching mm/dd/yyyy date format using regex expression
-                if (date.matches(" ^(1[0-2]|0[1-9])/(3[01]|[12][0-9]|0[1-9])/[0-9]{4}$")) {
-                    return date;
-                }
-            } catch (NoSuchElementException ex) {
-                System.out.println("Error reading date, please try again");
+        while (true) {
+            String date = readInput();
+
+            //Matching mm/dd/yyyy date format using regex expression
+            if (date.matches("^(1[0-2]|0[1-9])/(3[01]|[12][0-9]|0[1-9])/[0-9]{4}$")) {
+                return date;
             }
+
+            System.out.println("The date is in the wrong format: Please try again");
         }
     }
 }
