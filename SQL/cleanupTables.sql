2537cd092379c7012c804b54de0db6b9185b6bb6
Andrew Ha <andrewha428@gmail.com>
2020-04-25 23:00:13 -0400

    Added queries for adding constraints to tables

--- SQL/cleanupTables.sql
+++ SQL/cleanupTables.sql
@@ -13,3 +13,14 @@ delete from branches where branch_id not in (select branch_id from has_customers
 
 -- Make sql delete statement for deleting customers that do not appear in owns_account
 delete from customer where customer_id not in (select customer_id from owns_account)
+
+-- Add constraint for credit_card so that balance and balance due are <= credit limit
+ALTER TABLE credit_card
+ADD CHECK (balance_due <= credit_limit)
+
+ALTER TABLE credit_card
+ADD CHECK (balance <= credit_limit);
+
+-- Add constraint to balance for accounts
+ALTER TABLE account
+ADD CHECK (balance >= 0);
