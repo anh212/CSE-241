f5cabc741a31dddedc21e1ba9c6775ee4ed429e1
Andrew Ha <andrewha428@gmail.com>
2020-04-26 21:33:19 -0400

    Included function to clear console

--- src/Input.java
+++ src/Input.java
@@ -77,4 +77,9 @@ public class Input {
             System.out.println("The date is in the wrong format: Please try again");
         }
     }
+
+    public static void clearConsole() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 }
