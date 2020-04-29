306eb994a4f5aea88fcc05c3979d2d3858e06a9a
Andrew Ha <andrewha428@gmail.com>
2020-04-29 01:43:46 -0400

    Removed unnecessary imports

--- src/AccountInterface.java
+++ src/AccountInterface.java
@@ -1,12 +1,4 @@
 import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Date;
 
 public class AccountInterface {
     public static void Interface(Customer customer, Connection conn) {
